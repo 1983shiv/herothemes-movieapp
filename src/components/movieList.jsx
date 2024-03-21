1736d26228dca08391a21f8690c5582f40212671
= <=>
2024-03-21 14:31:10 +0530

    added env variable

--- src/components/movieList.jsx
+++ src/components/movieList.jsx
@@ -1,6 +1,6 @@
 import { useSelector, useDispatch } from "react-redux";
 import { selectMovie, fetchMovieAsync, selectMovieStatus } from "../reducers/movieReducer";
-import { selectUsers, updateUsers, fetchUserAsync, selectUserstatus } from "../reducers/userReducer";
+import { selectUsers, fetchUserAsync, selectUserstatus } from "../reducers/userReducer";
 import { addFavorite, removeFavorite, selectMylist } from "../reducers/mylistReducer";
 import { useEffect, useState } from "react";
 
@@ -106,33 +106,6 @@ const MovieList = () => {
     </>
   );
 
-  // return (
-  //   <div className="flex flex-col m-8 w-4/5">
-  //     {status === 'loading' ? (
-  //       <p>Loading...</p>
-  //     ) : (
-  //       <ul className="flex flex-col">
-  //         {movies.map((movie) => (
-  //           <li className="flex flex-row justify-between p-2 my-2 bg-gray-200 border rounded-md" key={movie.ID}>
-  //             <h1 className="text-4xl">{movie.post_title}</h1>
-  //             {/* <p className="my-2 text-xl">{movie.post_content}</p> */}
-  //             <p className="my-8 text-lg flex items-center">
-  //               {userstatus === 'loading' ? '' : (
-  //                 <span onClick={() => handleAddRemoveFav(movie.ID, users.name)}>
-  //                   {(mylist.some(fav => fav.movie_id === movie.ID)) ? (
-  //                     <FaHeart className="text-red-500 cursor-pointer" />
-  //                   ) : (
-  //                     <FaRegHeart className="text-red-500 cursor-pointer" />
-  //                   )}
-  //                 </span>
-  //               )}
-  //             </p>
-  //           </li>
-  //         ))}
-  //       </ul>
-  //     )}
-  //   </div>
-  // );
 }
 
 export default MovieList
--- src/reducers/movieAPI.js
+++ src/reducers/movieAPI.js
@@ -1,8 +1,9 @@
 import axios from "axios";
+const API_URL = import.meta.env.VITE_MOVIE_API_URL
 
 export const fetchMovie = async () => {
     try{
-      let response = await axios.get("http://localhost/gpes/wp-json/herothemes/v1/movies");
+      let response = await axios.get(API_URL);
       if (response.status === 200) {
         return response.data;
       }
